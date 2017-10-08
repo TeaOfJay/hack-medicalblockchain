0ed762e0d2110ab27589829308f25b63822ac8bf
csclm <entropiccipher@gmail.com>
2017-10-08 07:09:40 -0500

    muh backend

--- my-network/lib/updateRecord.js
+++ my-network/lib/updateRecord.js
@@ -1,12 +1,80 @@
 /**
- * Track the trade of a commodity from one trader to another
- * @param {org.blockchainboyz.record.UpdateRecord} transact 
+ * @param {org.blockchainboyz.medicalrecord.UpdateRecord} transact 
  * @transaction
 */
 function UpdateRecord(transact) {
-  var iso = transact.updateTime;
-  transact.record.entry = iso;
-  return getAssetRegistry('org.blockchainboyz.record.MedicalRecord')
+  
+  transact.record.data = transact.newData;
+  transact.record.format = transact.newFormat;
+  
+  return getAssetRegistry('org.blockchainboyz.medicalrecord.MedicalRecord')
+        .then(function (assetRegistry) {
+            return assetRegistry.update(transact.record);
+        });
+  
+}
+
+/**
+ * @param {org.blockchainboyz.medicalrecord.TransferRecord} TransferRecord
+ * @transaction
+*/
+function TransferRecord(transact) {
+  
+  transact.record.providers = transact.newProvider;
+  
+  return getAssetRegistry('org.blockchainboyz.medicalrecord.MedicalRecord')
+        .then(function (assetRegistry) {
+            return assetRegistry.update(transact.record);
+        });
+  
+}
+
+
+/**
+ * @param {org.blockchainboyz.medicalrecord.CreateRecord} transact 
+ * @transaction
+*/
+function CreateRecord(transact) {
+  
+  var newrecord = {
+    	
+  };
+  
+  return getAssetRegistry('org.blockchainboyz.medicalrecord.MedicalRecord')
+        .then(function (assetRegistry) {
+            return assetRegistry.update(transact.record);
+        });
+  
+}
+
+/**
+ * @param {org.blockchainboyz.medicalrecord.AddTrustedProvider} transact 
+ * @transaction
+*/
+function AddTrustedProvider(transact) {
+  
+  if(!transact.patient.trustedProviders.find(function(x){return x==transact.provider;}))
+    	transact.patient.trustedProviders.push(transact.provider);
+  
+  return getAssetRegistry('org.blockchainboyz.medicalrecord.MedicalRecord')
+        .then(function (assetRegistry) {
+            return assetRegistry.update(transact.record);
+        });
+  
+}
+
+
+/**
+ * @param {org.blockchainboyz.medicalrecord.AddTrustedProvider} transact 
+ * @transaction
+*/
+function RemoveTrustedProvider(transact) {
+  
+  
+  var p = transact.patient.trustedProviders
+  p = p.filter(function(x){return x!=transact.provider;});
+  
+  return getAssetRegistry('org.blockchainboyz.medicalrecord.MedicalRecord')
         .then(function (assetRegistry) {
             return assetRegistry.update(transact.record);
         });
